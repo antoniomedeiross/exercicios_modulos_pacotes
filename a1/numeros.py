30dd94122f51d284916e706b3cbe215c707c629e
Antonio Medeiros <antoniomedeirosdev@gmail.com>
2023-04-14 19:44:18 -0300

    Add files via upload

---
+++ a1/numeros.py
@@ -0,0 +1,7 @@
+import uteis
+
+num = int(input('dígite um número: '))
+
+print(f'o fatorial de {num} é {uteis.fatorial(num)}')
+print(f'o dobro de {num} é {uteis.dobro(num)}')
+print(f'o triplo de {num} é {uteis.triplo(num)} ')
